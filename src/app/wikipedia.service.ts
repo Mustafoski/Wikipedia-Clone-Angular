1180460ac458b49357e0fada69f9d0e00ef30e78
Emir Mustafoski <emirmustafoski92@gmail.com>
2021-12-06 14:03:09 +0100

    add1

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,8 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { pluck } from 'rxjs/operators';
 
-
+interface IWikipediaResponse {
+  query: {
+    search: {
+      title: string;
+      snippet: string;
+      pageid: number;
+    }[];
+  };
+}
 
 @Injectable({
   providedIn: 'root',
@@ -11,15 +20,17 @@ export class WikipediaService {
   constructor(private http: HttpClient) {}
 
   search(term: string): Observable<any> {
-    return this.http.get('https://en.wikipedia.org/w/api.php?', {
-      params: {
-        action: 'query',
-        format: 'json',
-        list: 'search',
-        utf8: '1',
-        srsearch: term,
-        origin: '*',
-      },
-    });
+    return this.http
+      .get('https://en.wikipedia.org/w/api.php?', {
+        params: {
+          action: 'query',
+          format: 'json',
+          list: 'search',
+          utf8: '1',
+          srsearch: term,
+          origin: '*',
+        },
+      })
+      .pipe(pluck('query', 'search'));
   }
 }
